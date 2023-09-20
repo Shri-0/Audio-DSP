c95aefcddd04f4a3386307610797da1a209b40f1
Shri-0 <shrinath.narayanan@gmail.com>
2023-09-19 19:22:49 -0700

    "sine wave equation"

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
+
+import numpy as np
+import wave
+import struct
+import matplotlib.pyplot as plt
+
+
+
+
+
+
+
+
 #frequency - number of times a sine wave repeats in a second. We will use 1k
+frequency = 1000
+
 #sampling rate - let's take 48
+num_samples = 48000
 
 # sine wave formula : y(t) = A * sin(2 * pi * f * t)
 
@@ -8,3 +24,14 @@
 # pi is pi
 # f is frequency
 # t is our sample
+
+#sampling rate
+sampling_rate = 48000.0
+amplitude = 16000
+
+file = "test.wav"
+
+
+#function
+sine_wave = [np.sin(2 * np.pi * frequency * x/sampling_rate)
+             for x in range(num_samples)]
