bbec7eb1023bfd57c431e8a3ba8fdd438dd786c0
Shri-0 <shrinath.narayanan@gmail.com>
2023-09-19 19:17:07 -0700

    "info"

---
+++ main.py
@@ -0,0 +1,10 @@
+#frequency - number of times a sine wave repeats in a second. We will use 1k
+#sampling rate - let's take 48
+
+# sine wave formula : y(t) = A * sin(2 * pi * f * t)
+
+# y(t) is the axis we want to sample to calculate x axis sample t
+# A is amplitude
+# pi is pi
+# f is frequency
+# t is our sample
